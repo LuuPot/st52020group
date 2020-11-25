e382dfc07f439482e40f66eb7da6371d51479e69
potluu <50609453+potluu@users.noreply.github.com>
2020-11-25 16:14:47 +0700

    s

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  moduleNameMapper: {
-    '\\.(css|less|sass|scss)$': '<rootDir>/mocks/styleMock.js'
-  }
-}
--- server/utils/index.js
+++
@@ -1,4 +0,0 @@
-export { default as sortBy } from './sort'
-export { default as onlyDemo } from './demo'
-export { addFields, commentCount } from './fields'
-export { findReply, updateReplies, deleteReply as deleteReplyArr, sortReplies } from './replies'
